666e59e5344524d320f8084dde857d6b5a38f011
AgentMeap <huudatnguyen15@gmail.com>
2025-03-05 01:05:13 +0700

    add thymeleaf, lombok

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,22 +1,11 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-
-//   @RequestMapping("/")
-//   public String getHomePage() {
-//     return "hello from controller";
-//   }
-// }
-
-@RestController
+@Controller
 public class UserController {
 
   private UserService userService;
@@ -25,8 +14,9 @@ public class UserController {
     this.userService = userService;
   }
 
-  @GetMapping("/")
+  @RequestMapping("/")
   public String getHomePage() {
-    return userService.handleHello();
+    String test = this.userService.handleHello();
+    return "home_page.html";
   }
 }
