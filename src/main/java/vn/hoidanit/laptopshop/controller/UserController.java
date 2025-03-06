aab9cdab1a2bfd7c074d54d115b9fc0f3f12f1b8
AgentMeap <huudatnguyen15@gmail.com>
2025-03-06 16:01:15 +0700

    add user controller

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,22 +1,45 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
 
-  private UserService userService;
+    private UserService userService;
 
-  public UserController(UserService userService) {
-    this.userService = userService;
-  }
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-  @RequestMapping("/")
-  public String getHomePage() {
-    String test = this.userService.handleHello();
-    return "home_page.html";
-  }
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("test", test);
+        model.addAttribute("meap", "From controller with model");
+        return "home_page";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        // Add a User object to the model for form binding
+        model.addAttribute("user", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(@ModelAttribute("user") User user, Model model) {
+        // Handle the submitted user data
+        System.out.println("Received user: " + user);
+
+        // Add user back to model for form redisplay
+        model.addAttribute("user", user);
+        return "home_page";
+    }
 }
