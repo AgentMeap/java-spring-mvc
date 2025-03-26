9d369a662d3f2ece4a054b30c0008d7fda68f6d9
AgentMeap <huudatnguyen15@gmail.com>
2025-03-26 17:10:58 +0700

    add entities

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 import jakarta.persistence.Entity;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
